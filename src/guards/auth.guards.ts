0ca351c7392e32fc9362a21d15eac99528d7c03c
lakmal05 <lakmaljayawardhana59@gmail.com>
2023-07-07 12:25:40 +0530

    authorization

--- src/guards/auth.guards.ts
+++ src/guards/auth.guards.ts
@@ -1,8 +1,58 @@
-import { CanActivate } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as jwt from 'jsonwebtoken';
 
+interface JWTPayload {
+  name: string;
+  id: number;
+  iat: number;
+  exp: number;
+}
 
-export class AuthGuard implements CanActivate{
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly prismaService: PrismaService,
+  ) {}
 
+  async canActivate(context: ExecutionContext) {
+    const roles = this.reflector.getAllAndOverride('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
-    
+    if (roles?.length) {
+      const request = context.switchToHttp().getRequest();
+      const token = request.headers?.authorization?.split('Bearer ')[1];
+
+      try {
+        const payload = (await jwt.verify(
+          token,
+          process.env.JSON_WEB_TOKEN_KEY,
+        )) as JWTPayload;
+
+        const user = await this.prismaService.user.findUnique({
+          where: {
+            id: payload.id,
+          },
+        });
+
+        if (!user) {
+          return false;
+        }
+        
+        console.log({user});
+        
+
+        if (roles.includes(user.user_type)) {
+          return true;
+        }
+      } catch (error) {
+        return error;
+      }
+    }
+  }
 }
